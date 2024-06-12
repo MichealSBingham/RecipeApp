58eb5f4372c75fd243f1bf90fe370102f7ac76a5
Micheal S. Bingham <micheal.s.bingham@icloud.com>
2024-06-12 13:26:55 -0400

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Micheal Bingham on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
